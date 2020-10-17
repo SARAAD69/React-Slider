a3cfdd6d36ab693f8e55bb1f5608da48f658f4e5
Artemii Chernega <a.chernega2000@gmail.com>
2020-10-17 14:14:43 +0300

    Added slide navigation

--- src/components/SliderComponent.js
+++ src/components/SliderComponent.js
@@ -14,18 +14,36 @@ function Slider() {
         <ImgComponent src={image2} />, 
         <ImgComponent src={image3} />,
         <ImgComponent src={image4} />,
-        <ImgComponent src={image5} />
+        <ImgComponent src={image5} />,
+        <p>This is test to show that it's not obligatory to have image in slide</p>
     ]
+    let buttons = []
+    const buttonlist = (i) => {
+        for (let i = 1; i <= sliderArr.length; i++) {
+        buttons.push(<button className="navigation" key={i} onClick={(e) => {e = parseInt(e.target.innerHTML); moveTo(e)}}>{i}</button>);
+          }
+          return buttons;
+    }
     const [x, setX] = useState(0)
     const showPrev = () => {
         x === 0 ? setX(-100 * (sliderArr.length - 1)) : setX(x + 100);
+        console.log(x);
     }
     const showNext = () => {
         x === -100 * (sliderArr.length - 1) ? setX(0) : setX(x - 100);
+        console.log(x);
+    }
+    const moveTo = (e) => {
+        console.log(x)
+        setX(-100*(e-1))
     }
 
+
     return (
-        <div className="slider">
+        <div className="slider">        
+            <div id="outer">
+                {buttonlist(1)}
+            </div>            
         {
             sliderArr.map((item, index) => {
                 return (
